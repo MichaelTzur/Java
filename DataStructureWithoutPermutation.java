550f0a5b8ba15c25674cccc4981e2211f7b39e6e
MichaelTzur <59913971+MichaelTzur@users.noreply.github.com>
2020-06-06 10:18:26 +0300

    Add files via upload

---
+++ DataStructureWithoutPermutation.java
@@ -0,0 +1,32 @@
+package permutation;
+
+import java.util.*;
+
+public class DataStructureWithoutPermutation {
+
+    //In map, the key value is unique.
+    private static Map<String, Integer> map = new HashMap<>();
+
+    private static void addNumToMap(int num) {
+        //separate digits
+        String[] numDigits = String.valueOf(num).split("");
+        //sort digits
+        Arrays.sort(numDigits);
+        //combine digits
+        String strNum = String.join("", numDigits);
+        //add to map
+        map.put(strNum, num);
+    }
+
+//    public static void main(String[] args) {
+//
+//        addNumToMap(1230);
+//        addNumToMap(1203);
+//        addNumToMap(1023);
+//
+//        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+//            System.out.println("key=" + entry.getKey() + ", value=" + entry.getValue());
+//        }
+//        System.out.println("Map size: " + map.size());
+//    }
+}
