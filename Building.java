0d314a5c8b6b98b713c5f78ab28a8b267e2e00c1
MichaelTzur <59913971+MichaelTzur@users.noreply.github.com>
2020-06-06 19:35:37 +0300

    Add files via upload

---
+++ Building.java
@@ -0,0 +1,58 @@
+package building;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class Building {
+
+    private Floor[] floors;
+
+    public Building() {
+        this.floors = new Floor[4];
+        Arrays.setAll(floors, i -> new Floor());
+    }
+
+    public Floor[] getFloors() {
+        return floors;
+    }
+
+    public void fillBuildingWithMockData() {
+        for (Floor floor : floors) {
+            for (Room room : floor.getRooms()) {
+                fillRoomWithMockData(room);
+            }
+        }
+    }
+
+    private void fillRoomWithMockData(Room room) {
+
+        Person[] parents = room.getParents();
+        Person[] children = room.getChildren();
+
+        if (parents.length == 1) {
+            parents[0] = new Man(fillPersonWithMockData());
+        }
+        else if (parents.length == 2) {
+            parents[0] = new Man(fillPersonWithMockData());
+            parents[1] = new Woman(fillPersonWithMockData());
+
+            Arrays.setAll(children, i -> new Child(fillPersonWithMockData()));
+        }
+        room.setParents(parents);
+        room.setChildren(children);
+    }
+
+    private Person fillPersonWithMockData() {
+
+        String[] names = {"Asaf", "John", "Tal", "Sapir", "David"};
+        Person.Gender[] genders = Person.Gender.values();
+        int age;
+
+        Random random = new Random();
+        String name = names[random.nextInt(5)];
+        Person.Gender gender = genders[random.nextInt(2)];
+        age = random.nextInt(90);
+
+        return new Person(name, gender, age);
+    }
+}
